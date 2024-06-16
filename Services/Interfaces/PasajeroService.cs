c791080a427ea452120d987968d1396b67bfedc5
José Leonardo Zúñiga Hernández <114005429+Leonides2@users.noreply.github.com>
2024-06-15 20:35:58 -0600

    update

---
+++ Services/Interfaces/PasajeroService.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Entities.Models;
+
+namespace Services.Interfaces
+{
+    public class PasajeroService
+    {
+        private readonly ExamenContext _context;
+
+        public PasajeroService(ExamenContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
